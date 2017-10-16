dd5cf7428743c9f92c56f1d51259d5a91bfc1a42
komantnick <sayapin6@mail.ru>
2017-10-17 00:11:43 +0300

    Изменения

---
+++ lib/db/data-example.php
@@ -0,0 +1,18 @@
+<?php
+require_once('database.php');
+/**
+ * There is an example of usage of database class of Capella
+ */
+$database = new DatabaseWorker();
+$ip = $database->getUserIP();
+$database_name = 'capella-storage';
+
+try {
+    $database->query('INSERT INTO ' . $database_name . ' (id,extension,ip) VALUES (:id,:extension,:ip)');
+    $database->bind(':id', $id);
+    $database->bind(':extension', $extension);
+    $database->bind(':ip', $ip);
+    $database->execute();
+} catch (PDOException $e) {
+    echo 'Error in loading data to database: ' . $e->getMessage();
+}
